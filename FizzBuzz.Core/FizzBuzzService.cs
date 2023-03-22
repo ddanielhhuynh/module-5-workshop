02b9055ff689da5715cb4a834f74a395ce382243
Daniel Huynh <ddanielhhuynh@gmail.com>
2023-03-22 18:32:18 -0400

    added test

--- FizzBuzz.Core/FizzBuzzService.cs
+++ FizzBuzz.Core/FizzBuzzService.cs
@@ -1,5 +1,31 @@
 ﻿namespace FizzBuzz.Core;
 public class FizzBuzzService
 {
+    public string Print(int number)
+    {
+        if(IsDivisibleByThreeAndFive(number)){
+            return "FizzBuzz";
+        }
+        else if(IsDivisibleByThree(number)){
+            return "Fizz";
+        }else if (IsDivisibleByFive(number)){
+            return "Buzz";
+        }
+        return number.ToString();
+    }
+
+    private bool IsDivisibleByThree(int number)
+    {
+        return number % 3 == 0;
+    }
+
+    private bool IsDivisibleByFive(int number)
+    {
+        return number % 5 == 0;
+    }
+    private bool IsDivisibleByThreeAndFive(int number)
+    {
+        return number % 3 == 0 && number % 5 == 0;
+    }
 
 }
